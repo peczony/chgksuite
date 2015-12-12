d58375527356372a47898777aa2ed513089d724e
Alexander Pecheny <peczony@gmail.com>
2015-12-12 18:54:46 +0300

    dedent

--- chgksuite.py
+++ chgksuite.py
@@ -17,57 +17,57 @@ import tkFont
 debug = False
 
 def gui_choose_action(args):
-        ch_defaultauthor = IntVar()
-        if args.defaultauthor:
+    ch_defaultauthor = IntVar()
+    if args.defaultauthor:
+        ch_defaultauthor.set(1)
+    else:
+        ch_defaultauthor.set(0)
+    def parsereturn():
+        root.ret = 'parse', ch_defaultauthor.get()
+        root.quit()
+        root.destroy()
+    def parsedirreturn():
+        root.ret = 'parsedir', ch_defaultauthor.get()
+        root.quit()
+        root.destroy()
+    def composereturn():
+        root.ret = 'compose', ch_defaultauthor.get()
+        root.quit()
+        root.destroy()
+    def toggle_da():
+        if ch_defaultauthor.get() == 0:
             ch_defaultauthor.set(1)
         else:
             ch_defaultauthor.set(0)
-        def parsereturn():
-            root.ret = 'parse', ch_defaultauthor.get()
-            root.quit()
-            root.destroy()
-        def parsedirreturn():
-            root.ret = 'parsedir', ch_defaultauthor.get()
-            root.quit()
-            root.destroy()
-        def composereturn():
-            root.ret = 'compose', ch_defaultauthor.get()
-            root.quit()
-            root.destroy()
-        def toggle_da():
-            if ch_defaultauthor.get() == 0:
-                ch_defaultauthor.set(1)
-            else:
-                ch_defaultauthor.set(0)
-        root = Tk()
-        root.eval('tk::PlaceWindow {} center'.format(
-            root.winfo_pathname(root.winfo_id())))
-        root.ret = 'None', '0'
-        root.grexit = lambda: on_close(root)
-        root.protocol("WM_DELETE_WINDOW", root.grexit)
-        frame = Frame(root)
-        frame.pack()
-        bottomframe = Frame(root)
-        bottomframe.pack(side = 'bottom')
-        Button(frame, command=
-            parsereturn, text = 'Parse file').pack(side = 'left', 
-            padx = 20, pady = 20,
-            ipadx = 20, ipady = 20,)
-        Button(frame, command=
-            parsedirreturn, text = 'Parse directory').pack(side = 'left', 
-            padx = 20, pady = 20,
-            ipadx = 20, ipady = 20,)
-        Button(frame, command=
-            composereturn, text = 'Compose').pack(side = 'left',
-            padx = 20, pady = 20,
-            ipadx = 20, ipady = 20,)
-        da = Checkbutton(bottomframe, text='Default author while parsing',
-            variable=ch_defaultauthor, command=toggle_da)
-        if args.defaultauthor:
-            da.select()
-        da.pack(side = 'bottom')
-        root.mainloop()
-        return root.ret
+    root = Tk()
+    root.eval('tk::PlaceWindow {} center'.format(
+        root.winfo_pathname(root.winfo_id())))
+    root.ret = 'None', '0'
+    root.grexit = lambda: on_close(root)
+    root.protocol("WM_DELETE_WINDOW", root.grexit)
+    frame = Frame(root)
+    frame.pack()
+    bottomframe = Frame(root)
+    bottomframe.pack(side = 'bottom')
+    Button(frame, command=
+        parsereturn, text = 'Parse file').pack(side = 'left', 
+        padx = 20, pady = 20,
+        ipadx = 20, ipady = 20,)
+    Button(frame, command=
+        parsedirreturn, text = 'Parse directory').pack(side = 'left', 
+        padx = 20, pady = 20,
+        ipadx = 20, ipady = 20,)
+    Button(frame, command=
+        composereturn, text = 'Compose').pack(side = 'left',
+        padx = 20, pady = 20,
+        ipadx = 20, ipady = 20,)
+    da = Checkbutton(bottomframe, text='Default author while parsing',
+        variable=ch_defaultauthor, command=toggle_da)
+    if args.defaultauthor:
+        da.select()
+    da.pack(side = 'bottom')
+    root.mainloop()
+    return root.ret
 
 def main():
     parser = argparse.ArgumentParser()
