fab9c6f0fed713eccd618269ee4672cafb11cb1b
Alexander Pecheny <peczony@gmail.com>
2015-12-12 18:54:26 +0300

    add defaultauthor support in gui

--- chgksuite.py
+++ chgksuite.py
@@ -5,7 +5,7 @@ import argparse
 import os
 import pdb
 from chgk_parser import gui_parse
-from chgk_composer import gui_compose
+from chgk_composer import gui_compose, on_close
 
 try:
     from Tkinter import Tk, Frame, IntVar, Button, Checkbutton, Entry, Label
@@ -16,23 +16,35 @@ import tkFont
 
 debug = False
 
-def gui_choose_action():
+def gui_choose_action(args):
+        ch_defaultauthor = IntVar()
+        if args.defaultauthor:
+            ch_defaultauthor.set(1)
+        else:
+            ch_defaultauthor.set(0)
         def parsereturn():
-            root.ret = 'parse'
+            root.ret = 'parse', ch_defaultauthor.get()
             root.quit()
             root.destroy()
         def parsedirreturn():
-            root.ret = 'parsedir'
+            root.ret = 'parsedir', ch_defaultauthor.get()
             root.quit()
             root.destroy()
         def composereturn():
-            root.ret = 'compose'
+            root.ret = 'compose', ch_defaultauthor.get()
             root.quit()
             root.destroy()
+        def toggle_da():
+            if ch_defaultauthor.get() == 0:
+                ch_defaultauthor.set(1)
+            else:
+                ch_defaultauthor.set(0)
         root = Tk()
         root.eval('tk::PlaceWindow {} center'.format(
             root.winfo_pathname(root.winfo_id())))
-        root.ret = 'None'
+        root.ret = 'None', '0'
+        root.grexit = lambda: on_close(root)
+        root.protocol("WM_DELETE_WINDOW", root.grexit)
         frame = Frame(root)
         frame.pack()
         bottomframe = Frame(root)
@@ -49,6 +61,11 @@ def gui_choose_action():
             composereturn, text = 'Compose').pack(side = 'left',
             padx = 20, pady = 20,
             ipadx = 20, ipady = 20,)
+        da = Checkbutton(bottomframe, text='Default author while parsing',
+            variable=ch_defaultauthor, command=toggle_da)
+        if args.defaultauthor:
+            da.select()
+        da.pack(side = 'bottom')
         root.mainloop()
         return root.ret
 
@@ -69,9 +86,15 @@ def main():
     parser.add_argument('--password', '-p')
     parser.add_argument('--community', '-c')
     args = parser.parse_args()
+
+    root = Tk()
+    root.withdraw()
     
     if not args.action:
-        args.action = gui_choose_action()
+        args.action, args.defaultauthor = gui_choose_action(args)
+        args.defaultauthor = str(args.defaultauthor)
+    if args.defaultauthor in {'0', 'False'}:
+        args.defaultauthor = 0
     if args.action == 'parse':
         gui_parse(args)
     if args.action == 'parsedir':
