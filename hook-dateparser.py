7d570d4bb00c1b40dfaa80294f0333c02f0ac9c3
Alexander Pecheny <peczony@gmail.com>
2019-06-02 23:08:20 +0300

    add hook-dateparser

---
+++ hook-dateparser.py
@@ -0,0 +1,12 @@
+from PyInstaller.utils.hooks import exec_statement, collect_submodules
+
+# taken from here: https://github.com/pyinstaller/pyinstaller/issues/3429#issuecomment-379723204
+# the official hook in pyinstaller/master doesn't work
+
+strptime_data_file = exec_statement(
+"import inspect; import _strptime; print(inspect.getfile(_strptime))"
+)
+
+datas = [ (strptime_data_file, ".") ]
+
+hiddenimports = collect_submodules('dateparser')
