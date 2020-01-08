22c230a88a7cea92b221861e08d61d688247c9a4
Alexander Pecheny <peczony@gmail.com>
2020-01-09 00:06:49 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="chgksuite",
+    version="0.6",
+    packages=["chgksuite"],
+    entry_points={
+        "console_scripts": ["chgksuite = chgksuite.__main__:main"]
+    },
+)
