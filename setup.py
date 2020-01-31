042d9c368e2454821b764bf0d0b945fedcdaae52
Alexander Pecheny <peczony@gmail.com>
2020-02-01 01:11:42 +0300

    update setup and history

--- setup.py
+++ setup.py
@@ -8,12 +8,24 @@ def get_version():
     return version["__version__"]
 
 
+long_description = """**chgksuite** is an utility that helps chgk editors.
+
+[Chgk](https://en.wikipedia.org/wiki/What%3F_Where%3F_When%3F) (short for Chto? Gde? Kogda?) is a popular russian quiz.
+
+Project home on gitlab: https://gitlab.com/peczony/chgksuite
+
+Documentation (in Russian): https://peczony.gitbook.io/chgksuite
+"""
+
+
 setup(
     name="chgksuite",
     version=get_version(),
     author="Alexander Pecheny",
     author_email="peczony@gmail.com",
     description="A package for chgk automation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://gitlab.com/peczony/chgksuite",
     classifiers=[
         "Programming Language :: Python :: 3",
