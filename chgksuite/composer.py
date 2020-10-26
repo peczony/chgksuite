c32f03739e0fe52a4affa0516345e8b6319709dc
Alexander Pecheny <peczony@gmail.com>
2020-10-26 17:59:42 +0300

    bring back old behaviour: don't try to post security=private in communities

--- chgksuite/composer.py
+++ chgksuite/composer.py
@@ -922,7 +922,7 @@ def lj_post(stru, args, edit=False):
     community = args.community
     if community:
         add_params["usejournal"] = community
-    if args.security:
+    elif args.security:
         add_params["security"] = "usemask"
         add_params["allowmask"] = (
             "1" if args.security == "friends" else args.security
