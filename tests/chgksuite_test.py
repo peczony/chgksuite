ef4540e696b97c2af811904064a293cf9a50cf06
Alexander Pecheny <ap@pecheny.me>
2023-07-12 14:56:50 +0400

    parametrize tests & temporarily remove db test until db is fixed

--- tests/chgksuite_test.py
+++ tests/chgksuite_test.py
@@ -67,65 +67,66 @@ def normalize(string):
     return string.replace("\r\n", "\n")
 
 
-def test_canonical_equality(parsing_engine):
-    for filename in os.listdir(currentdir):
-        if filename.endswith(".canon"):
-            print(os.getcwd())
-            with make_temp_directory(dir=".") as temp_dir:
-                to_parse_fn = filename[:-6]
-                print(os.getcwd())
-                shutil.copy(os.path.join(currentdir, filename), temp_dir)
-                print(os.getcwd())
-                shutil.copy(os.path.join(currentdir, to_parse_fn), temp_dir)
-                print(os.getcwd())
-                print("Testing {}...".format(filename[:-6]))
-                print(os.getcwd())
-                bn, _ = os.path.splitext(to_parse_fn)
-                subprocess.call(
-                    [
-                        "python",
-                        "-m",
-                        "chgksuite",
-                        "parse",
-                        "--parsing_engine",
-                        parsing_engine,
-                        os.path.join(temp_dir, to_parse_fn),
-                    ]
-                )
-                with codecs.open(os.path.join(temp_dir, bn + ".4s"), "r", "utf8") as f:
-                    parsed = f.read()
-                with codecs.open(os.path.join(temp_dir, filename), "r", "utf8") as f:
-                    canonical = f.read()
-                assert normalize(canonical) == normalize(parsed)
+CANON_FILENAMES = [
+    fn for fn in os.listdir(currentdir) if fn.endswith(".canon")
+]
 
+@pytest.mark.parametrize("filename", CANON_FILENAMES)
+def test_canonical_equality(parsing_engine, filename):
+    print(os.getcwd())
+    with make_temp_directory(dir=".") as temp_dir:
+        to_parse_fn = filename[:-6]
+        print(os.getcwd())
+        shutil.copy(os.path.join(currentdir, filename), temp_dir)
+        print(os.getcwd())
+        shutil.copy(os.path.join(currentdir, to_parse_fn), temp_dir)
+        print(os.getcwd())
+        print("Testing {}...".format(filename[:-6]))
+        print(os.getcwd())
+        bn, _ = os.path.splitext(to_parse_fn)
+        call_args = [
+            "python",
+            "-m",
+            "chgksuite",
+            "parse",
+            "--parsing_engine",
+            parsing_engine,
+            os.path.join(temp_dir, to_parse_fn),
+        ]
+        subprocess.call(call_args, timeout=5)
+        with codecs.open(os.path.join(temp_dir, bn + ".4s"), "r", "utf8") as f:
+            parsed = f.read()
+        with codecs.open(os.path.join(temp_dir, filename), "r", "utf8") as f:
+            canonical = f.read()
+        assert normalize(canonical) == normalize(parsed)
 
-def test_docx_composition():
-    for filename in os.listdir(currentdir):
-        if (
-            filename.endswith((".docx", ".txt"))
-            and filename == "Kubok_knyagini_Olgi-2015.docx"
-        ):
-            print("Testing {}...".format(filename))
-            with make_temp_directory(dir=".") as temp_dir:
-                shutil.copy(os.path.join(currentdir, filename), temp_dir)
-                temp_dir_filename = os.path.join(temp_dir, filename)
-                parsed = workaround_chgk_parse(temp_dir_filename)
-                file4s = os.path.splitext(filename)[0] + ".4s"
-                composed_abspath = os.path.join(temp_dir, file4s)
-                print(composed_abspath)
-                with codecs.open(composed_abspath, "w", "utf8") as f:
-                    f.write(compose_4s(parsed, args=DefaultArgs()))
-                code = subprocess.call(
-                    [
-                        "python",
-                        "-m",
-                        "chgksuite",
-                        "compose",
-                        "docx",
-                        composed_abspath,
-                    ]
-                )
-                assert 0 == code
+TO_DOCX_FILENAMES = [
+    fn for fn in os.listdir(currentdir) if fn.endswith((".docx", ".txt"))
+]
+TO_DOCX_FILENAMES.remove("balt09-1.txt")  # TODO: rm this line once dns is fixed
+
+@pytest.mark.parametrize("filename", TO_DOCX_FILENAMES)
+def test_docx_composition(filename):
+    print("Testing {}...".format(filename))
+    with make_temp_directory(dir=".") as temp_dir:
+        shutil.copy(os.path.join(currentdir, filename), temp_dir)
+        temp_dir_filename = os.path.join(temp_dir, filename)
+        parsed = workaround_chgk_parse(temp_dir_filename)
+        file4s = os.path.splitext(filename)[0] + ".4s"
+        composed_abspath = os.path.join(temp_dir, file4s)
+        print(composed_abspath)
+        with codecs.open(composed_abspath, "w", "utf8") as f:
+            f.write(compose_4s(parsed, args=DefaultArgs()))
+        call_args = [
+            "python",
+            "-m",
+            "chgksuite",
+            "compose",
+            "docx",
+            composed_abspath,
+        ]
+        code = subprocess.call(call_args, timeout=5)
+        assert 0 == code
 
 
 @pytest.mark.tex
